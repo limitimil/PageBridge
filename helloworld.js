042311537bddf1262da58295343bb9e69b2d3322
limitimil <zlsh10340@gmail.com>
2020-03-10 12:07:42 +0800

    add delay to prevent jira secondary overwrite

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,7 @@
 (document.body || document.documentElement).addEventListener(
   "transitionend",
   async () => {
+    await delay();
     let anchors = getJenkinsAnchors();
     anchors.forEach(elem => {
       elem.style.backgroundColor = "red";
