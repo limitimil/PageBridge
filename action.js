b9c2d83ea0607fc39cca5d02cbf51672382b71ba
limitimil <zlsh10340@gmail.com>
2020-04-05 03:04:23 +0800

    ACTION: handle button event with js style

--- action.js
+++ action.js
@@ -1,4 +1,9 @@
 let dom = document.getElementById('jira-target');
+
+document.getElementById('submit').addEventListener('click', ()=> {
+  console.log('submit here')
+});
+
 chrome.tabs.getSelected(null, (tab)=> {
   let paths = getPathsFromUrl(tab.url);
   for (let path of paths){
