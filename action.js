1499dbcacb7b6bcab9e88b90301c1c7be63faff0
limitimil <zlsh10340@gmail.com>
2020-04-05 03:24:20 +0800

    ACTION: 3rd: use axios to handle put and send comment appending request to backend

--- action.js
+++ action.js
@@ -1,10 +1,18 @@
 let dom = document.getElementById('jira-target');
-
+let current_url = '';
 document.getElementById('submit').addEventListener('click', ()=> {
   console.log('submit here')
+  axios({
+    method: 'put',
+    baseURL: serviceConfigurations.jenkinsPortalMasterRoot,
+    url: '/append_comment/ci_reference',
+    'Content-Type': 'application/json',
+    data: { issue_key: dom.value, reference_url: current_url}
+  })
 });
 
 chrome.tabs.getSelected(null, (tab)=> {
+  current_url = tab.url;
   let paths = getPathsFromUrl(tab.url);
   for (let path of paths){
     if (isJiraIssueKey(path)){
