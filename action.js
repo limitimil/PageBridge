78e98e8bfb2ceed1ab76f93913c68894daf009ce
limitimil <zlsh10340@gmail.com>
2020-04-05 03:34:11 +0800

    ACTION: handle the ux of api call

--- action.js
+++ action.js
@@ -1,14 +1,19 @@
 let dom = document.getElementById('jira-target');
 let current_url = '';
-document.getElementById('submit').addEventListener('click', ()=> {
-  console.log('submit here')
+document.getElementById('submit').addEventListener('click', function(){
+  const submitter = this;
+  submitter.disabled = true;
   axios({
     method: 'put',
     baseURL: serviceConfigurations.jenkinsPortalMasterRoot,
     url: '/append_comment/ci_reference',
     'Content-Type': 'application/json',
     data: { issue_key: dom.value, reference_url: current_url}
-  })
+  }).then(()=> { 
+    submitter.innerText = 'success';
+  }).catch(()=>{
+    submitter.innerText = 'fail';
+  });
 });
 
 chrome.tabs.getSelected(null, (tab)=> {
