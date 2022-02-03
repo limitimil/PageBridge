56c344df1c79865e0a6c11a3be48a6f81d242e0d
limitimil <zlsh10340@gmail.com>
2022-02-03 22:43:33 +0800

    Content Reader: responsible chain

--- contentReader.js
+++ contentReader.js
@@ -1,12 +1,46 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  const regex = new RegExp(request.regex);
-  // TODO: responsible chain
-  // title
+class ChainDetector {
+  constructor(){
+    this.ruleFunctions = []
+  }
+  // TODO: add type hint
+  // for the following functions
+  // - push
+  // - detectAndSendResponse
+  // - ruleMatchInTitle
+  push(method) {
+    this.ruleFunctions = [
+      ...this.ruleFunctions,
+      method
+    ];
+  }
+  detectAndSendResponse({ regex, sendResponse }){
+    if(this.ruleFunctions.length === 0) {
+      throw Error('please push before using detect');
+    }
+    for( const rf of this.ruleFunctions ) {
+      const result = rf(regex, sendResponse);
+      if(result) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+function ruleMatchInTitle(regex, sendResponse) {
   const matchInTheTitle = document.title.match(regex);
   if(matchInTheTitle) {
     sendResponse({jiraId: matchInTheTitle[0] });
     return true;
   }
+}
+const chain = new ChainDetector();
+chain.push(ruleMatchInTitle);
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const regex = new RegExp(request.regex);
+  // TODO: responsible chain
+  // title
+  return chain.detectAndSendResponse({ regex, sendResponse });
   // url
   // headers
   // anchors
